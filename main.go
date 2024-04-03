efb4307fb5405a69928c593282b1bd8bb35a67c8
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-03 13:11:22 -0400

    add endpoints with http servemux

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	err := dotenv.Load()
 	if err != nil {
 		log.Fatal("error loading .env file")
@@ -17,4 +18,16 @@ func main() {
 	port := os.Getenv("PORT")
 	mux := http.NewServeMux()
 
+	handlerFileserver := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/*", handlerFileserver)
+	mux.HandleFunc("/v1/readiness", handlerReadiness)
+	mux.HandleFunc("/v1/err", handlerError)
+
+	corsMux := middlewareCors(mux)
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+	log.Printf("Serving on port: %v", port)
+	log.Fatal(server.ListenAndServe())
 }
