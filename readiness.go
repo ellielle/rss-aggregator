7cca319e27751f2c246700eb7df2bbc723dd1382
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-03 13:11:56 -0400

    add endpoints to check json responses

---
+++ readiness.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	type response struct {
+		Status string `json:"status"`
+	}
+	respondWithJSON(w, http.StatusOK, response{Status: "ok"})
+}
+
+func handlerError(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
