864ae4e91b939984d65fd3b880fceabfec996a5a
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-04 06:09:51 -0400

    add route handling and api struct for db

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 
 	dotenv "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
+	"github.com/ellielle/rss-aggregator/internal/database"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+	mu *sync.Mutex
+}
 
 func main() {
 	const filepathRoot = "."
@@ -14,14 +24,23 @@ func main() {
 	if err != nil {
 		log.Fatal("error loading .env file")
 	}
-
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("error connecting to database")
+	}
+
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{DB: dbQueries}
+
 	mux := http.NewServeMux()
 
 	handlerFileserver := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
-	mux.Handle("/app/*", handlerFileserver)
-	mux.HandleFunc("/v1/readiness", handlerReadiness)
-	mux.HandleFunc("/v1/err", handlerError)
+	mux.Handle("GET /app/*", handlerFileserver)
+	mux.HandleFunc("GET /v1/readiness", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerError)
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
 
 	corsMux := middlewareCors(mux)
 	server := &http.Server{
