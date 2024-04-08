86b193dc17bb36c6f8923f932ba06a3886e7ec01
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-07 20:49:16 -0400

    add an index to api_key to search on faster

--- sql/schema/002_apikey.sql
+++ sql/schema/002_apikey.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 ALTER TABLE IF EXISTS users
 ADD COLUMN API_KEY VARCHAR(64) NOT NULL DEFAULT encode(sha256(random()::text::bytea), 'hex'); 
+CREATE INDEX IF NOT EXISTS api_key_idx on users (api_key);
 
 -- +goose Down
 ALTER TABLE users
 DROP COLUMN IF EXISTS API_KEY;
-
+DROP INDEX IF EXISTS api_key_idx;
