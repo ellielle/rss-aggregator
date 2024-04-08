6756332e15a5202623cc6b8bb45197368fa48dac
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-08 19:21:30 -0400

    change query to :exec

--- sql/queries/feeds_follows.sql
+++ sql/queries/feeds_follows.sql
@@ -1,9 +1,12 @@
--- name: CreateFeedsFollow :one
+-- name: CreateFeedsFollows :one
 INSERT INTO feeds_follows (id, created_at, updated_at, user_id, feed_id)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
--- name: DeleteFeedsFollow :one
+-- name: DeleteFeedsFollows :exec
 DELETE FROM feeds_follows
-WHERE id = $1
-RETURNING *;
+WHERE id = $1;
+
+-- name: ListFeedsFollows :many
+SELECT * FROM feeds_follows
+WHERE user_id = $1;
