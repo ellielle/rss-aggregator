c5c03c27e247f3e234cc3b5085516504c99b9ac6
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-09 08:00:35 -0400

    fix: use auth middleware instead of checking in function

--- handler_users.go
+++ handler_users.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"context"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
+
+	"github.com/ellielle/rss-aggregator/internal/database"
 )
 
 // Handler to find a user via API key
-func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request, user database.User) {
 	defer r.Body.Close()
 	type response struct {
 		Id        uuid.UUID `json:"id"`
@@ -20,25 +20,11 @@ func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
 		ApiKey    string    `json:"api_key"`
 	}
 
-	// Get API key from headers
-	apiKey, found := strings.CutPrefix(r.Header.Get("Authorization"), "ApiKey ")
-	if !found {
-		respondWithError(w, http.StatusBadRequest, "Missing authorization header")
-		return
-	}
-
-	// Look user up by API key
-	user, err := cfg.DB.GetUserByApiKey(context.Background(), apiKey)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "No user found")
-		return
-	}
-
 	respondWithJSON(w, http.StatusOK, response{
 		Id:        user.ID,
 		Name:      user.Name,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
-		ApiKey:    apiKey,
+		ApiKey:    user.ApiKey,
 	})
 }
