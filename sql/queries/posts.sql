58627de1d297d8dc5d8f26d81ee80a86a8bd858d
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-11 12:16:20 -0400

    fix sql query to return only posts data

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -4,7 +4,9 @@ VALUES ($1, $2, $3, $4, $5, $6, $7)
 RETURNING *;
 
 -- name: GetPostsByUser :many
-SELECT * FROM posts
-JOIN users ON users.id = $1
+SELECT posts.* FROM posts
+JOIN feeds ON posts.feed_id = feeds.id
+JOIN feeds_follows ON feeds.id = feeds_follows.feed_id
+WHERE feeds_follows.user_id = $1
 ORDER BY posts.updated_at DESC
 LIMIT $2;
